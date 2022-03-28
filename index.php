824c29e368e73d900dcc2adb0cf0ae100bb9e9cc
Horrilla <nevajnooru@gmail.com>
2022-03-28 18:11:35 +0300

    pdo mysql

--- index.php
+++ index.php
@@ -14,38 +14,7 @@ if (!$query->execute()) {
 	print_r($response);
 	echo '</pre>';
 }
-//echo '<pre>';
-//print_r($query->errorInfo());
-//echo '</pre>';
 
-/*$age = 23;
-
-function sayYes() {
-	echo "<br>";
-	echo 'Yes';
-}
-sayYes();
-
-Определние констант
-const STATE = 3.14;
-define("STATE", 3.14);
-echo STATE;
-
-Итерполяция
-$quantity = 10;
-$some_string = "I have " . STATE . " cars";
-
-echo $some_string;
-
-var_dump(STATE);
-
-
-$string = <<<HERE
-I'm $age years old
-Are u?
-HERE;
-
-echo $string;*/
 
 
 
