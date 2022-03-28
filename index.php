d2bac9aa8a66e9230da16a6f4bcf5fffe29a90c0
Horrilla <nevajnooru@gmail.com>
2022-03-28 18:43:14 +0300

    pdo mysql

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 
+$data = [
+	'login' => '  <script>vova</script>  ',
+	'password' => 'asdqQYY1238OIEK)))'
+];
+$data['login'] = strip_tags(trim($data['login']));
+$data['password'] = htmlspecialchars(trim($data['password']));
+
 $db = new PDO('mysql:host=127.0.0.1;dbname=clients;', 'root', '');
-$query = $db->prepare("SELECT * FROM cliets");
+$query = $db->prepare("INSERT INTO users SET login=:login, password=:password");
+$query->execute($data);
+$response = $query->fetchAll();
+
+
+$query = $db->prepare("SELECT * FROM users");
 $query->execute();
 $response = $query->fetchAll();
 
