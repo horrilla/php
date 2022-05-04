cb0b6fba9f502979b2385d006b12ebcdf8424a22
Horrilla <nevajnooru@gmail.com>
2022-05-04 15:37:35 +0300

    коммит гостевой книги

--- guests/functions.php
+++ guests/functions.php
@@ -1,1 +1,31 @@
 <?php
+
+
+function post_message() {
+
+	try {
+		$name = htmlspecialchars($_POST['guest']);
+		$message = htmlspecialchars($_POST['text']);
+		$query = "INSERT INTO guests (name, text) VALUES (:name, :text)";
+		global $pdo;
+
+		$stmt = $pdo->prepare($query);
+		$stmt->execute(['name' => $name, 'text' => $message]);
+		$pdo = null;
+	} catch (PDOException $e) {
+		echo 'Ошибка запроса:' . $e->getMessage();
+	}
+}
+
+function get_message() {
+
+	try {
+		global $pdo;
+		$query = "SELECT name, text, date FROM guests ORDER BY id DESC";
+		$stmt = $pdo->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	} catch (PDOException $e) {
+		echo 'Ошибка запроса:' . $e->getMessage();
+	}
+}
--- guests/index.php
+++ guests/index.php
@@ -1,1 +1,70 @@
 <?php
+
+require_once 'functions.php';
+require_once 'connect.php';
+
+if (!empty($_POST)) {
+	post_message();
+	header("Location: {$_SERVER['PHP_SELF']}");
+	exit;
+}
+
+$messages = get_message();
+
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Гости</title>
+
+	<style>
+        .messages {
+            margin: 15px auto;
+            width: auto;
+            border: 1px solid black;
+        }
+
+        .author {
+            margin: 5px 0 0 10px;
+            font-size: 18px;
+            font-weight: bold;
+        }
+
+        .message {
+            margin: 5px 0 10px 10px;
+            font-size: 17px;
+        }
+	</style>
+
+</head>
+<body>
+<form action="index.php" method="post">
+	<p>
+		<label for="name">Имя</label><br>
+		<input type="text" name="guest" id="name">
+	</p>
+	<p>
+		<label for="text">Сообщение</label><br>
+		<textarea name="text" id="text" cols="30" rows="10"></textarea>
+	</p>
+	<p>
+		<button name="btn" type="submit">Отправить</button>
+	</p>
+	<hr>
+	<?php if (!empty($messages)): ?>
+		<?php foreach ($messages as $message): ?>
+			<div class="messages">
+				<p class="author">Автор: <?= $message['name']; ?>, Дата: <?= $message['date']; ?></p>
+				<div class="message">
+					<?= nl2br($message['text']); ?>
+				</div>
+			</div>
+		<?php endforeach; ?>
+	<?php endif; ?>
+</form>
+</body>
+</html>
