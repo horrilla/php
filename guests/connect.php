5dc259e83a08fcfdf792a26cc1228403e04fdcba
horrilla <43247616+horrilla@users.noreply.github.com>
2022-05-13 14:32:54 +0300

    Add files via upload

---
+++ guests/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'localhost';
+$db = 'guestbook';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+];
+
+
+try {
+	$pdo = new PDO($dsn, $user, $pass, $opt);
+	return $pdo;
+} catch (PDOException $e) {
+	echo 'Connection failed:' . $e->getMessage() . '<br>';
+}
